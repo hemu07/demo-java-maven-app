4d4c04b58cd9aed3b9474dd3e0af10b9d3bc494f
hemu7 <hemalijayswal07@gmail.com>
2024-04-24 23:15:37 +0530

    updated the image version

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJarFile() {
 def buildImage() {
     echo "building the docker image.."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t hemu07/hemali_repo:jma-3.0 .'
+    sh 'docker build -t hemu07/hemali_repo:jma-4.0 .'
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push hemu07/hemali_repo:jma-3.0'
+    sh 'docker push hemu07/hemali_repo:jma-4.0'
     }
 }
 
