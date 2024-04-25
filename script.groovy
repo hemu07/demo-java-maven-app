d8df8ee11a98299689367a6150d9f21416e51d7d
hemu07 <90203539+hemu07@users.noreply.github.com>
2024-04-25 23:22:46 +0530

    Update script.groovy
    removed functions defined in shared library

--- script.groovy
+++ script.groovy
@@ -1,18 +1,3 @@
-def buildJarFile() {
-    echo "Building the application..."  
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image.."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t hemu07/hemali_repo:jma-5.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push hemu07/hemali_repo:jma-5.0'
-    }
-}
-
-
 def deployApp() {
     echo "Deploying the application..."
 }
