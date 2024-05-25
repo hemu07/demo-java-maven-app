bc9c7dac69cb3ca1379c4684cf5d4c4c16956cef
hemu07 <90203539+hemu07@users.noreply.github.com>
2024-05-26 02:42:42 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -30,13 +30,13 @@ def commitVersionUpdate() {
     withCredentials([
                     usernamePassword(credentialsId: 'github', usernameVariable: 'USER', passwordVariable: 'PAT')
                     ]) {
-                    sh "git config --global user.email "jenkins@example.com"'
-                    sh "git config --global user.name "jenkins"'
-                    sh "git status"
-                    sh "git branch"
-                    sh "git config --list"
+                    sh 'git config --global user.email "jenkins@example.com"'
+                    sh 'git config --global user.name "jenkins"'
+                    sh 'git status'
+                    sh 'git branch'
+                    sh 'git config --list'
                     sh "git remote set-url origin https://${USER}:${PAT}@github.com/hemu07/demo-java-maven-app.git"
-                    sh "git add ."
+                    sh 'git add .'
                     sh 'git commit -m "ci:version bump"'
                     sh 'git push origin HEAD:jenkins-job'
                     }
